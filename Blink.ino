68c3afdfa2be108069540397916ef01f53d9c13d
Milinparekh <milinparekh93@gmail.com>
2018-03-03 10:35:13 +0530

    two led from one led now
    here now we have learnt so far that arduino can easily handle  the single led now we are going to make it live for the demo for the multiple led interface

--- Blink.ino
+++ Blink.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(11,OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -33,5 +34,8 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(1000); 
+  digitaWrtie(11,HIGH);
+  delay(1000);
+  digitalWrite(11,LOW);// wait for a second
 }
